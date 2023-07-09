25a1997426cf05bcc1cd9657ea2dbabb2a914a46
fe1w0 <xzasliuxinrong@gmail.com>
2023-07-10 01:17:04 +0800

    Coding: 0.2.a version.

--- Programs/utils/handle_file_factory/parse_java_file.py
+++ Programs/utils/handle_file_factory/parse_java_file.py
@@ -9,25 +9,29 @@ def extract_functions(java_file):
         source_code = file.read()
 
     tree = javalang.parse.parse(source_code)  # type: javalang.ast.Node
+    lines = source_code.split('\n')
     
     function_bodies = {}
     functions_names = []
 
-    for path, node in tree.filter(javalang.tree.MethodDeclaration):
-        function_name = node.name
-        node_copy = node  # type: javalang.tree.MethodDeclaration
-        # print(type(node))
-        # print(type(node.body))
-        pos = node.position  # type: javalang.tokenizer.Position
-        # print(type(pos))
-        start_line, start_column = node.position  # type: javalang.ast.Node.position
+    for path, node in tree.filter(javalang.tree.ClassDeclaration):
+        # pos = node.position
+        start_line, start_col = node.position
+        class_declaration = lines[start_line - 1]
 
-        # print("start_line:", start_line, "start_column:", start_column)
-        function_body = extract_function_body(source_code, start_line, start_column, node.body).encode()
-        
-        function_bodies[function_name] = {"function_name": function_name, "function_body": function_body}
-        functions_names.append(function_name)
+        for child in node.body:
 
+            # if isinstance(child, javalang.tree.FieldDeclaration):
+            #     fields.append(lines[child.position[0] - 1])
+            if isinstance(child, javalang.tree.MethodDeclaration):
+                start_line, start_column = child.position
+                function_body = extract_function_body(source_code, start_line, start_column, child.body)
+                function_name = child.name
+                
+                function_body = class_declaration + "\n" + function_body + "\n}"
+                
+                function_bodies[function_name] = {"function_name": function_name, "function_body": function_body.encode()}
+                functions_names.append(function_name)
     return function_bodies, functions_names
 
 
@@ -56,7 +60,7 @@ def extract_function_body(source_code, start_line, start_column, body):
                 break
             end_line += 1
 
-    function_lines = lines[start_line - 1: end_line + 1]
+    function_lines = lines[start_line - 1: end_line + 2]
 
     # function_lines[0] = function_lines[0][start_column - 1:]
     # print(function_lines[0])
@@ -69,8 +73,25 @@ def extract_function_body(source_code, start_line, start_column, body):
     return function_body
 
 
-def test():
-    file_name = "DataSet/CommitsCollection/Java/hyperledger_besu/a9bdd29128eca70d1922b282aa2b479ab492b563/ethereum_api_src_main_java_org_hyperledger_besu_ethereum_api_jsonrpc_internal_methods_JsonRpcMethod.java"
-    functions, functions_name  = extract_functions(file_name)
-    print(functions)
-    print(functions_name)
+# def test():
+#     file_name = "DataSet/CommitsCollection/Java/snakeyaml_snakeyaml/4b8d1af4bd422a015fbb07d235f282412c863220/src_test_java_org_yaml_snakeyaml_issues_issue525_FuzzyStackOverflowTest.java"
+#     file_name = "DataSet/CommitsCollection/Java/eclipse-californium_californium/9112803a20152d1386415a4418e639919f8ac12b/scandium-core_src_main_java_org_eclipse_californium_scandium_DTLSConnector.java"
+#     functions, functions_name  = extract_functions(file_name)
+#     print(functions['stop'])
+#     # print(functions_name)
+
+# test()
+
+# import javalang
+
+# with open(file_name, 'r') as file:
+#     java_source = file.read()
+
+# tree = javalang.parse.parse(java_source)
+
+# for path, node in tree.filter(javalang.tree.ClassDeclaration):
+#     print(f"Class: {node.name}")
+#     for field in node.fields:
+#         print(f"    Field: {field.type} {field.declarators[0].name}")
+#     for method in node.methods:
+#         print(f"    Method: {method.name}({', '.join(str(arg.type) for arg in method.parameters)})")
